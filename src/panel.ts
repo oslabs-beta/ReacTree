2494196c74af0771553bc3bce8da8a9371c88080
Kun Ok Noh <bnohcub@hotmail.com>
2023-02-07 16:04:44 -0800

    working panel

--- src/panel.ts
+++ src/panel.ts
@@ -1,45 +1,60 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
-import { getNonce } from "./getNonce";
+import { getNonce } from './getNonce';
 // import { Parser } from './parser';
 // import { Tree } from "./types/Tree";
 
 export default class ReacTreePanel {
-    
+  // parser: Parser | undefined;
+  // private readonly _extensionUri: vscode.Uri;
+  // private readonly context: vscode.ExtensionContext;
+
   public static currentPanel: ReacTreePanel | undefined;
 
-  private static readonly viewType = "reacTree";
+  private static readonly viewType = 'reacTree';
 
   private readonly _panel: vscode.WebviewPanel;
   private readonly _extensionPath: string;
   private _disposables: vscode.Disposable[] = [];
 
   public static createOrShow(extensionPath: string) {
-    const column = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
-      
+    const column = vscode.window.activeTextEditor
+      ? vscode.window.activeTextEditor.viewColumn
+      : undefined;
+
     // If we already have a panel, show it.
     // Otherwise, create a new panel.
     if (ReacTreePanel.currentPanel) {
       ReacTreePanel.currentPanel._panel.reveal(column);
     } else {
-    // ReactPanel.currentPanel = new ReactPanel(extensionPath, column || vscode.ViewColumn.One);
-      console.log('FIRST')
-      ReacTreePanel.currentPanel = new ReacTreePanel(extensionPath, vscode.ViewColumn.Two);
+      // ReactPanel.currentPanel = new ReactPanel(extensionPath, column || vscode.ViewColumn.One);
+      console.log('FIRST');
+      ReacTreePanel.currentPanel = new ReacTreePanel(
+        extensionPath,
+        vscode.ViewColumn.Two
+      );
     }
   }
 
   private constructor(extensionPath: string, column: vscode.ViewColumn) {
-    console.log('SECOND')
+    console.log('SECOND');
     this._extensionPath = extensionPath;
 
     // Create and show a new webview panel
-    this._panel = vscode.window.createWebviewPanel(ReacTreePanel.viewType, "ReacTree", column, {
-      // Enable javascript in the webview
-      enableScripts: true,
+    this._panel = vscode.window.createWebviewPanel(
+      ReacTreePanel.viewType,
+      'ReacTree',
+      column,
+      {
+        // Enable javascript in the webview
+        enableScripts: true,
 
-      // And restric the webview to only loading content from our extension's `media` directory.
-      localResourceRoots: [vscode.Uri.file(path.join(this._extensionPath, "out"))],
-    });
+        // And restric the webview to only loading content from our extension's `media` directory.
+        localResourceRoots: [
+          vscode.Uri.file(path.join(this._extensionPath, 'out')),
+        ],
+      }
+    );
 
     // Set the webview's initial html content
     this._panel.webview.html = this._getHtmlForWebview();
@@ -47,30 +62,30 @@ export default class ReacTreePanel {
     // Listen for when the panel is disposed
     // This happens when the user closes the panel or when the panel is closed programatically
     this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
-      
+
     this._panel.webview.onDidReceiveMessage(
       (msg: any) => {
         switch (msg.command) {
           case 'startup':
-            console.log('message received')
+            console.log('message received');
             // vscode.commands.executeCommand('vscode-note.note.edit', msg.data.id, msg.data.category);
             break;
           case 'testing':
-            console.log('reachedBrain')
+            console.log('reachedBrain');
             this._panel!.webview.postMessage({ command: 'refactor' });
             break;
-          case 'edit-contentFile':
-            vscode.commands.executeCommand('vscode-note.note.edit.col.content', msg.data.id, msg.data.n);
+          case 'onFile': {
+            console.log('received file');
+            // Edge case if the user sends in nothing
+            if (!msg.value) {
+              return;
+            }
+            // Run an instance of the parser
+            // this.parser = new Parser(msg.value);
+            // this.parser.parse();
+            // this.updateView();
             break;
-          case 'edit-col':
-            vscode.commands.executeCommand('vscode-note.note.edit.col', msg.data.id, msg.data.cn);
-            break;
-          case 'doc':
-            vscode.commands.executeCommand('vscode-note.note.doc.show', msg.data);
-            break;
-          case 'files':
-            vscode.commands.executeCommand('vscode-note.note.files.open', msg.data);
-            break;
+          }
         }
       },
       null,
@@ -78,12 +93,23 @@ export default class ReacTreePanel {
     );
   }
 
+  // private updateView() {
+  //   // Save current state of tree to workspace state:
+  //   const tree = this.parser.getTree();
+  //   this.context.workspaceState.update('reactree', tree);
+  //   // Send updated tree to webview
+  //   this._view.webview.postMessage({
+  //     type: "parsed-data",
+  //     value: tree
+  //   });
+  // }
+
   public dispose() {
     ReacTreePanel.currentPanel = undefined;
-        console.log('dipose part 1')
+    console.log('dipose part 1');
     // Clean up our resources
     this._panel.dispose();
-        console.log('dipose part 2')
+    console.log('dipose part 2');
 
     while (this._disposables.length) {
       const x = this._disposables.pop();
@@ -94,8 +120,14 @@ export default class ReacTreePanel {
   }
 
   private _getHtmlForWebview() {
-    const scriptPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, "out", 'main.wv.js'));
-    const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
+    const scriptPathOnDisk = vscode.Uri.file(
+      path.join(this._extensionPath, 'out', 'main.wv.js')
+    );
+    const stylePathOnDisk = vscode.Uri.file(
+      path.join(this._extensionPath, '/src/webview/style.css')
+    );
+    const styleUri = stylePathOnDisk.with({ scheme: 'vscode-resource' });
+    const scriptUri = scriptPathOnDisk.with({ scheme: 'vscode-resource' });
 
     // Use a nonce to whitelist which scripts can be run
     const nonce = getNonce();
@@ -106,6 +138,7 @@ export default class ReacTreePanel {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>reacttree</title>
+        <link rel="stylesheet" href="${styleUri}">
       </head>
       <body>
         <div id="root"></div>
--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,39 +1,161 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-interface vscode {
-    postMessage(message: any): void;
-}
-// declare function acquireVsCodeApi(): vscode;
-declare const vscode: vscode;
+import { useCallback } from 'react';
+import ReactFlow, {
+  Node,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  ConnectionLineType,
+  Edge,
+} from 'reactflow';
+// import './style.css';
+// import dagre from 'dagre';
+// import '../dagre.css';
 
-const sendMessage = () => {
-    console.log('button clicked')
-    vscode.postMessage({ command: 'testing' });
-}
+const initialNodes: Node[] = [
+  {
+    id: '1',
+    type: 'input',
+    data: {
+      label: (
+        <div>
+          <strong>hello</strong>
+          <div>sadfsdf</div>
+        </div>
+      ),
+    },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '3',
+    data: { label: 'Node 3' },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '4',
+    data: { label: 'Node 4' },
+    position: { x: 0, y: 0 },
+    type: 'custom',
+  },
+];
+
+const initialEdges: Edge[] = [
+  {
+    id: 'e1-2',
+    source: '1',
+    target: '2',
+    type: 'smoothstep',
+    animated: true,
+  },
+  {
+    id: 'e1-3',
+    source: '1',
+    target: '3',
+    type: 'smoothstep',
+    animated: true,
+  },
+  {
+    id: 'e3-4',
+    source: '3',
+    target: '4',
+    type: 'smoothstep',
+    animated: false,
+  },
+];
+
+// const dagreGraph = new dagre.graphlib.Graph();
+// dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+// const getLayoutedElements = (nodes: any[], edges: any[], direction = 'TB') => {
+//   const isHorizontal = direction === 'LR';
+//   dagreGraph.setGraph({ rankdir: direction });
+
+//   nodes.forEach((node) => {
+//     dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+//   });
+
+//   edges.forEach((edge) => {
+//     dagreGraph.setEdge(edge.source, edge.target);
+//   });
+
+//   dagre.layout(dagreGraph);
+
+//   nodes.forEach((node) => {
+//     const nodeWithPosition = dagreGraph.node(node.id);
+//     node.targetPosition = isHorizontal ? 'left' : 'top';
+//     node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+//     // We are shifting the dagre node position (anchor=center center) to the top left
+//     // so it matches the React Flow node anchor point (top left).
+//     node.position = {
+//       x: nodeWithPosition.x - nodeWidth / 2,
+//       y: nodeWithPosition.y - nodeHeight / 2,
+//     };
+
+//     return node;
+//   });
+
+//   return { nodes, edges };
+// };
+
+// const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+//   initialNodes,
+//   initialEdges
+// );
 
 function App() {
-    const [buttonText, setButtonText] = React.useState('The brain is pending');
-    
-    React.useEffect(() => {
-        window.addEventListener('message', event => {
-          const message = event.data; // The json data that the extension sent
-          switch (message.command) {
-              case 'refactor':
-                  setButtonText('The brain is working');
-                  break;
-          }
-        });
-      }, );
-    
-    return (
-        <div>
-            <h1>Functional Components Work!</h1>
-            <button onClick={sendMessage}>{buttonText}</button>
-        </div>
-    );
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Edge<any> | Connection) =>
+      setEdges((eds) =>
+        addEdge(
+          { ...params, type: ConnectionLineType.SmoothStep, animated: true },
+          eds
+        )
+      ),
+    []
+  );
+//   const onLayout = useCallback(
+//     (direction: string | undefined) => {
+//       const { nodes: layoutedNodes, edges: layoutedEdges } =
+//         getLayoutedElements(nodes, edges, direction);
+
+//       setNodes([...layoutedNodes]);
+//       setEdges([...layoutedEdges]);
+//     },
+//     [nodes, edges]
+//   );
+
+  return (
+    <div className="layoutflow">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        connectionLineType={ConnectionLineType.SmoothStep}
+        fitView
+      />
+      {/* <div className="controls">
+        <button onClick={() => onLayout('TB')}>vertical layout</button>
+        <button onClick={() => onLayout('LR')}>horizontal layout</button>
+      </div> */}
+    </div>
+  );
 }
-
-
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
