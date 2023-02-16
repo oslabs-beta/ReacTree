3fae324ffad5bb3bc34bdbc900fc62609163a784
Kun Ok Noh <bnohcub@hotmail.com>
2023-02-16 11:59:20 -0800

    implemented testing for the extension and parser, set up boiler plate for vs code testing
    Co-authored-by: justin27kim <justin27kim@users.noreply.github.com>

---
+++ out/webview/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require('react');
+const ReactDOM = require('react-dom');
+require("./style.css");
+require("./dagre.css");
+// Component import
+const Sidebar_1 = require("./components/Sidebar");
+ReactDOM.render(React.createElement(Sidebar_1.default, null), document.getElementById('root'));
+//# sourceMappingURL=index.js.map
