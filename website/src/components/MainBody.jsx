14099b2f6b8e2a5659d54c203d8cbae9cc781d08
Kevin <kd8liu@gmail.com>
2023-02-21 16:12:13 -0800

    updated website with gifs and css
    Co-authored-by: justin27kim <justin27kim@users.noreply.github.com>

--- website/src/components/MainBody.jsx
+++ website/src/components/MainBody.jsx
@@ -12,7 +12,11 @@ const MainBody = () => {
         <h1>ReacTree
           <img className='reactree-logo' src={ReacTree}></img>
         </h1>
-        <h3>VsCode React nagivation extension</h3>
+        <p className='sub-title'>Navigate and visualize your React application</p>
+        <br></br> 
+        <p className='reactree-intro'>
+          Traverse your React project with a visual hierarchical tree
+        </p>
         <br></br> 
         <div className='get-reactree'>
           <a className='get-button' href='https://marketplace.visualstudio.com/'>ReacTree
@@ -23,6 +27,7 @@ const MainBody = () => {
           </a>
         </div>
       </div>
+      
       <div className='features-main'>
         <h2>Features</h2>
         <div className='feature'>
@@ -30,15 +35,31 @@ const MainBody = () => {
             <div className='feature-title'>
               <h3>feature title</h3>
             </div>
-            <div className='feature-description'>feature description</div>
+            <div className='feature-description'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</div>
           </div>
           <img className='feature-image' src={LaunchPropsOpenFiles} alt="Launch reactree, show props, open files gif"/>
         </div>
+        
+        <div className='feature'>
+          <div className='feature-text'>
+            <div className='feature-title'>
+              <h3>feature title</h3>
+            </div>
+            <div className='feature-description'>feature description</div>
+          </div>
+          <img className='feature-image' src={NavbarControls} alt="Nav-bar controls gif"/>
+        </div>
 
+        <div className='feature'>
+          <div className='feature-text'>
+            <div className='feature-title'>
+              <h3>feature title</h3>
+            </div>
+            <div className='feature-description'>feature description</div>
+          </div>
+          <img className='feature-image' src={Themes} alt="Themes gif"/>
+        </div>
 
-
-        <img src={NavbarControls} alt="Nav-bar controls gif"/>
-        <img src={Themes} alt="Themes gif"/>
       </div>
     </div>
     
