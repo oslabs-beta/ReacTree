da1a2c0314c4c76d60874436703e778853386886
Kevin <kd8liu@gmail.com>
2023-02-23 12:34:45 -0800

    Installation image README

--- src/extension.ts
+++ src/extension.ts
@@ -2,28 +2,21 @@ import * as vscode from 'vscode';
 import ReacTreePanel from './panel';
 
 export function activate(extContext: vscode.ExtensionContext) {
-  // Create reacTree status bar button
-  const item = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Right
-  );
-
-  item.command = 'reacTree.startStatusBar';
-  item.tooltip = 'Activate ReacTree';
-  item.text = '$(type-hierarchy) Start Tree';
-  item.show();
-
   extContext.subscriptions.push(
     vscode.commands.registerCommand('reacTree.start', () => {
       ReacTreePanel.createOrShow(extContext);
     })
   );
-
-  extContext.subscriptions.push(
-    vscode.commands.registerCommand('reacTree.startStatusBar', () => {
-      ReacTreePanel.createOrShow(extContext);
-    })
+  
+  // Create reacTree status bar button
+  const item = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right
   );
-  vscode.commands.executeCommand('reacTree.startStatusBar');
+
+  item.command = 'reacTree.start';
+  item.tooltip = 'Activate ReacTree';
+  item.text = '$(type-hierarchy) Start Tree';
+  item.show();
 }
 
 export function deactivate() {}
